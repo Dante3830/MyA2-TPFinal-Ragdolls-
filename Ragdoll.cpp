c8a5b6b5d5ed138dc7a9ecae4430c980c936a812
Dante3830 <111542527+Dante3830@users.noreply.github.com>
2023-09-10 10:19:41 -0300

    Update Ragdoll.cpp

--- Ragdoll.cpp
+++ Ragdoll.cpp
@@ -15,72 +15,72 @@ Ragdoll::Ragdoll(Vector2f pos, b2World& World) {
 
 	//Posicionando el ragdoll
 	// Cabeza
-	bdyDef_rag[0].position = b2Vec2(pos.x, pos.y + 0.1f);
+	BdyDef_Rag[0].position = b2Vec2(pos.x, pos.y + 0.1f);
 	// Torso
-	bdyDef_rag[1].position = b2Vec2(pos.x, pos.y + 0.65f);
+	BdyDef_Rag[1].position = b2Vec2(pos.x, pos.y + 0.65f);
 	// Brazo Izq
-	bdyDef_rag[2].position = b2Vec2(pos.x - 0.32f, pos.y + 0.5f);
+	BdyDef_Rag[2].position = b2Vec2(pos.x - 0.32f, pos.y + 0.5f);
 	// Brazo Der
-	bdyDef_rag[3].position = b2Vec2(pos.x + 0.32f, pos.y + 0.5f);
+	BdyDef_Rag[3].position = b2Vec2(pos.x + 0.32f, pos.y + 0.5f);
 	// Pierna Izq
-	bdyDef_rag[4].position = b2Vec2(pos.x - 0.09f, pos.y + 1.5f); 
+	BdyDef_Rag[4].position = b2Vec2(pos.x - 0.09f, pos.y + 1.5f); 
 	// Pierna Der
-	bdyDef_rag[5].position = b2Vec2(pos.x + 0.09f, pos.y + 1.5f); 
+	BdyDef_Rag[5].position = b2Vec2(pos.x + 0.09f, pos.y + 1.5f); 
 
 	//Definimos al radgoll
 	for (int i = 0; i < 6; i++) {
-		bdyDef_rag[i].type = b2_dynamicBody;
-		bdy_rag[i] = World.CreateBody(&bdyDef_rag[i]);
+		BdyDef_Rag[i].type = b2_dynamicBody;
+		Bdy_Rag[i] = World.CreateBody(&BdyDef_Rag[i]);
 	}
 
 	for (int i = 0; i < 6; i++) {
-		fixDef_rag[i].shape = &shp_rag[i];
-		fixDef_rag[i].density = 0.1f;
-		fixDef_rag[i].friction = 0.5f;
-		fixDef_rag[i].restitution = 0.1f;
+		FixDef_Rag[i].shape = &shp_rag[i];
+		FixDef_Rag[i].density = 0.1f;
+		FixDef_Rag[i].friction = 0.5f;
+		FixDef_Rag[i].restitution = 0.1f;
 
-		fix_rag[i] = bdy_rag[i]->CreateFixture(&fixDef_rag[i]);
+		Fix_Rag[i] = Bdy_Rag[i]->CreateFixture(&FixDef_Rag[i]);
 	}
 
 	// Definimos sus resortes
 
 	// 1: Cabeza + Torso
-	jointRagDef[0].Initialize(bdy_rag[0], bdy_rag[1], b2Vec2(bdy_rag[0]->GetPosition().x, bdy_rag[0]->GetPosition().y + 0.08),
-		b2Vec2(bdy_rag[1]->GetPosition().x, bdy_rag[1]->GetPosition().y - 0.38));
+	JointRagDef[0].Initialize(Bdy_Rag[0], Bdy_Rag[1], b2Vec2(Bdy_Rag[0]->GetPosition().x, Bdy_Rag[0]->GetPosition().y + 0.08),
+		b2Vec2(Bdy_Rag[1]->GetPosition().x, Bdy_Rag[1]->GetPosition().y - 0.38));
 	// 2: Brazo Der + Torso
-	jointRagDef[1].Initialize(bdy_rag[1], bdy_rag[2], b2Vec2(bdy_rag[1]->GetPosition().x + 0.18, bdy_rag[1]->GetPosition().y - 0.38),
-		b2Vec2(bdy_rag[2]->GetPosition().x - 0.05, bdy_rag[2]->GetPosition().y - 0.28));
+	JointRagDef[1].Initialize(Bdy_Rag[1], Bdy_Rag[2], b2Vec2(Bdy_Rag[1]->GetPosition().x + 0.18, Bdy_Rag[1]->GetPosition().y - 0.38),
+		b2Vec2(Bdy_Rag[2]->GetPosition().x - 0.05, Bdy_Rag[2]->GetPosition().y - 0.28));
 	// 3: : Brazo Izq + Torso
-	jointRagDef[2].Initialize(bdy_rag[1], bdy_rag[3], b2Vec2(bdy_rag[1]->GetPosition().x - 0.18, bdy_rag[1]->GetPosition().y - 0.38),
-		b2Vec2(bdy_rag[3]->GetPosition().x + 0.05, bdy_rag[3]->GetPosition().y - 0.28));
+	JointRagDef[2].Initialize(Bdy_Rag[1], Bdy_Rag[3], b2Vec2(Bdy_Rag[1]->GetPosition().x - 0.18, Bdy_Rag[1]->GetPosition().y - 0.38),
+		b2Vec2(Bdy_Rag[3]->GetPosition().x + 0.05, Bdy_Rag[3]->GetPosition().y - 0.28));
 	// 4: Pierna Der + Torso
-	jointRagDef[3].Initialize(bdy_rag[1], bdy_rag[4], b2Vec2(bdy_rag[1]->GetPosition().x + 0.09, bdy_rag[1]->GetPosition().y + 0.38),
-		b2Vec2(bdy_rag[4]->GetPosition().x, bdy_rag[4]->GetPosition().y - 0.38));
+	JointRagDef[3].Initialize(Bdy_Rag[1], Bdy_Rag[4], b2Vec2(Bdy_Rag[1]->GetPosition().x + 0.09, Bdy_Rag[1]->GetPosition().y + 0.38),
+		b2Vec2(Bdy_Rag[4]->GetPosition().x, Bdy_Rag[4]->GetPosition().y - 0.38));
 	// 5: Pierna Izq + Torso
-	jointRagDef[4].Initialize(bdy_rag[1], bdy_rag[5], b2Vec2(bdy_rag[1]->GetPosition().x + 0.09, bdy_rag[1]->GetPosition().y + 0.38),
-		b2Vec2(bdy_rag[5]->GetPosition().x, bdy_rag[5]->GetPosition().y - 0.38));
+	JointRagDef[4].Initialize(Bdy_Rag[1], Bdy_Rag[5], b2Vec2(Bdy_Rag[1]->GetPosition().x + 0.09, Bdy_Rag[1]->GetPosition().y + 0.38),
+		b2Vec2(Bdy_Rag[5]->GetPosition().x, Bdy_Rag[5]->GetPosition().y - 0.38));
 
 	for (int i = 0; i < 5; i++) {
-		jointRagDef[i].damping = 0.3f;
-		jointRagDef[i].stiffness = 0.9f;
-		jointRagDef[i].collideConnected = true;
-		jointRagDef[i].length = 0.00025f;
-		jointRag[i] = (b2DistanceJoint*)World.CreateJoint(&jointRagDef[i]);
+		JointRagDef[i].damping = 0.3f;
+		JointRagDef[i].stiffness = 0.9f;
+		JointRagDef[i].collideConnected = true;
+		JointRagDef[i].length = 0.00025f;
+		JointRag[i] = (b2DistanceJoint*)World.CreateJoint(&JointRagDef[i]);
 	}
 
 	for (int i = 0; i < 6; i++) {
-		shape_rag[i] = new RectangleShape;
+		Shape_Rag[i] = new RectangleShape;
 	}
 
-	shape_rag[0]->setFillColor(Color::Color(0, 200, 0));
-	shape_rag[1]->setFillColor(Color::Yellow);
-	shape_rag[2]->setFillColor(Color::Red);
-	shape_rag[3]->setFillColor(Color::Red);
-	shape_rag[4]->setFillColor(Color::Blue);
-	shape_rag[5]->setFillColor(Color::Blue);
+	Shape_Rag[0]->setFillColor(Color::Color(0, 200, 0));
+	Shape_Rag[1]->setFillColor(Color::Yellow);
+	Shape_Rag[2]->setFillColor(Color::Red);
+	Shape_Rag[3]->setFillColor(Color::Red);
+	Shape_Rag[4]->setFillColor(Color::Blue);
+	Shape_Rag[5]->setFillColor(Color::Blue);
 
 	for (int i = 0; i < 6; i++) {
-		ragd[i] = new Object(bdy_rag[i], shape_rag[i]);
+		Ragd[i] = new Object(Bdy_Rag[i], Shape_Rag[i]);
 	}
 
 }
@@ -91,31 +91,31 @@ float Ragdoll::RadToDeg(float radians) {
 
 void Ragdoll::Draw(RenderWindow* _window) {
 	for (int i = 0; i < 6; i++) {
-		ragd[i]->Draw(_window);
+		Ragd[i]->Draw(_window);
 	}
 }
 
 void Ragdoll::ApplyForce(Vector2f _mousePos) {
 	for (int i = 0; i < 6; i++) {
-		bdy_rag[i]->ApplyForceToCenter(b2Vec2(_mousePos.x * 10, _mousePos.y * 10), false);
+		Bdy_Rag[i]->ApplyForceToCenter(b2Vec2(_mousePos.x * 10, _mousePos.y * 10), false);
 	}
 }
 
 //FloatRect Clone::getCloneBounds() {
 //	FloatRect CloneBounds;
 //	for (int i = 0; i < 6; i++) {
-//		CloneBounds = shape_rag[i]->getGlobalBounds();
+//		CloneBounds = Shape_Rag[i]->getGlobalBounds();
 //	}
 //	return CloneBounds;
 //}
 
 Vector2f Ragdoll::GetPosition(int i) {
-	b2Vec2 ClonePos = bdy_rag[i]->GetPosition();
+	b2Vec2 ClonePos = Bdy_Rag[i]->GetPosition();
 	return Vector2f(ClonePos.x, ClonePos.y);
 }
 
 Vector2f Ragdoll::GetSize(int i) const {
-	b2PolygonShape* shape = static_cast<b2PolygonShape*>(fix_rag[i]->GetShape());
+	b2PolygonShape* shape = static_cast<b2PolygonShape*>(Fix_Rag[i]->GetShape());
 	b2Vec2 size = shape->m_vertices[2] - shape->m_vertices[0];
 	return Vector2f(size.x, size.y);
 }
